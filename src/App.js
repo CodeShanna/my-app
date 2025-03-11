85ee7edef748b466ba089b30fe6fc4e0ace0e142
Shanna <shanna@jonesbunch.com>
2025-03-11 11:07:22 +0100

    Updated footer links

--- src/App.js
+++ src/App.js
@@ -5,8 +5,18 @@ export default function App() {
     <div className="App">
       <h1>Weather App</h1>
       <footer>
-        This project is open-sourced on GitHub and hosted on Netlify.
+        This project was coded by{" "}
+        <a href="https://shannajonesportfolio.netlify.app/">Shanna Jones</a>,
+        open-sourced on{" "}
+        <a
+          href="https://github.com/CodeShanna?tab=repositories"
+          target="_blank"
+        >
+          GitHub
+        </a>
+        and hosted on Netlify.
       </footer>
+      <br />
     </div>
   );
 }
