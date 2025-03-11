742eff846bff934929d6b2c07f7073463ba80137
Shanna <shanna@jonesbunch.com>
2025-03-11 14:05:17 +0100

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,20 +3,23 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by{" "}
-        <a href="https://shannajonesportfolio.netlify.app/">Shanna Jones</a>,
-        open-sourced on{" "}
-        <a
-          href="https://github.com/CodeShanna?tab=repositories"
-          target="_blank"
-        >
-          GitHub
-        </a>
-        and hosted on Netlify.
-      </footer>
-      <br />
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by{" "}
+          <a href="https://shannajonesportfolio.netlify.app/">Shanna Jones</a>,
+          open-sourced on{" "}
+          <a
+            href="https://github.com/CodeShanna?tab=repositories"
+            target="_blank"
+            rel="noreferrer"
+          >
+            GitHub
+          </a>
+          and hosted on Netlify.
+        </footer>
+        <br />
+      </div>
     </div>
   );
 }
